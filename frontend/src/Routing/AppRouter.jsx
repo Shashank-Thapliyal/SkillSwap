a39614773e5e98e4ce91972fbb3c30560a0df990
Shashank Thapliyal <shashank151002@gmail.com>
2025-07-03 20:41:15 +0530

    Add routing for requests, blocked users, proposals, and conversations

--- frontend/src/Routing/AppRouter.jsx
+++ frontend/src/Routing/AppRouter.jsx
@@ -5,28 +5,74 @@ import LogIn from '../pages/Login/LogIn';
 import Layout from "../pages/Layout";
 import EditProfile from "../pages/EditProfile/EditProfile";
 import ViewProfile from "../pages/ViewProfile/ViewProfile";
+import Dashboard from "../pages/dashboard/dashboard";
+import Discover from "../pages/Discover/Discover";
+import Connections from "../pages/Connections/Connections";
+import SentRequests from "../pages/ConnectionRequests/SentRequests";
+import ReceivedRequests from "../pages/ConnectionRequests/ReceivedRequests";
+import BlockedUsers from "../pages/BlockedUsers/BlockedUsers";
+import SendProposal from "../pages/Proposals/sendProposal";
+import ChatWrapper from "../pages/Conversation/ChatWrapper";
 
 const AppRouter = createBrowserRouter([
     {
         path: "/",
         element: <LandingPage />
-    }, 
+    },
     {
-        element : <Layout/>,
-        children:[ {
-            path : "/edit-profile",
-            element : <EditProfile />
-        },
-        {
-            path : "/profile/:userId",
-            element : <ViewProfile   isOwnProfile={false}/>
-        },{
-            path : "/profile",
-            element : <ViewProfile isOwnProfile={true} />
-        }
-    ]
+        element: <Layout />,
+        children: [
+            {
+                path: "/dashboard",
+                element: <Dashboard />
+            },
+            {
+                path: "/edit-profile",
+                element: <EditProfile />
+            },
+            {
+                path: "/profile/:userId",
+                element: <ViewProfile isOwnProfile={false} />
+            },
+            {
+                path: "/profile",
+                element: <ViewProfile isOwnProfile={true} />
+            }, {
+                path: "/discover",
+                element: <Discover />
+            }, {
+                path: "/connections",
+                element: <Connections />
+            },
+            {
+                path: "/requests",
+                children: [
+                  {
+                    path: "sent",
+                    element: <SentRequests />
+                  },
+                  {
+                    path: "received",
+                    element: <ReceivedRequests />
+                  }
+                ]
+            },{
+                path : "/blocked-users",
+                element : <BlockedUsers />
+            },{
+                path : "/send-proposal/:receiverId",
+                element : <SendProposal />
+            },{
+                path : "/conversations",
+                element : <ChatWrapper />
+            },{
+                path : "/conversations/:userId/:tab",
+                element : <ChatWrapper />
+            }
+              
+        ]
     }
-    ,{
+    , {
         path: "/signup",
         element: <SignUp />
     }, {
